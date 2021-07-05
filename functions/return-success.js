99d5d599d63d6a980d5b5a662da6e127a2a5fee3
Ramon Bezerra <ramonbezerra90@gmail.com>
2021-07-05 15:37:03 -0300

    fix(return-success): corrigindo fechamento da função

--- functions/return-success.js
+++ functions/return-success.js
@@ -1,34 +1,37 @@
 exports.handler = async (event, context) => {
+  const body = JSON.stringify({
+    title: "Nome do produto 10",
+    subTitle: "309825 | 987654321654 | Mix",
+    imgLink: "http://alkjlaksjdlkajskajslkdjsa.jpg",
+    imgSmallLink: "http://alkjlaksjdlkajskajslkdjsa.jpg",
+    info1: {
+      title: "Estoque",
+      subTitle: "35",
+    },
+    info2: {
+      title: "Custo",
+      subTitle: "R$ 7,59",
+    },
+    info3: {
+      title: "Valor",
+      subTitle: "R$ 263,45",
+    },
+    info4: {
+      title: "Últ. Entrada",
+      subTitle: "14/01/2021",
+    },
+    info5: {
+      title: "Últ. Saída",
+      subTitle: "05/03/2021",
+    },
+    info6: {
+      title: "Qtd. Dias",
+      subTitle: "-33",
+    },
+  });
+
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      title: "Nome do produto 10",
-      subTitle: "309825 | 987654321654 | Mix",
-      imgLink: "http://alkjlaksjdlkajskajslkdjsa.jpg",
-      imgSmallLink: "http://alkjlaksjdlkajskajslkdjsa.jpg",
-      info1: {
-        title: "Estoque",
-        subTitle: "35",
-      },
-      info2: {
-        title: "Custo",
-        subTitle: "R$ 7,59",
-      },
-      info3: {
-        title: "Valor",
-        subTitle: "R$ 263,45",
-      },
-      info4: {
-        title: "Últ. Entrada",
-        subTitle: "14/01/2021",
-      },
-      info5: {
-        title: "Últ. Saída",
-        subTitle: "05/03/2021",
-      },
-      info6: {
-        title: "Qtd. Dias",
-        subTitle: "-33",
-      }
-    })
+    body,
+  };
 };

7386b890f3b0586790769ffe8426b5cf0f123493
Ramon Bezerra <ramonbezerra90@gmail.com>
2021-07-05 15:12:17 -0300

    Commit inicial

---
+++ functions/return-success.js
@@ -0,0 +1,35 @@
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: {
+      title: "Nome do produto 10",
+      subTitle: "309825 | 987654321654 | Mix",
+      imgLink: "http://alkjlaksjdlkajskajslkdjsa.jpg",
+      imgSmallLink: "http://alkjlaksjdlkajskajslkdjsa.jpg",
+      info1: {
+        title: "Estoque",
+        subTitle: "35",
+      },
+      info2: {
+        title: "Custo",
+        subTitle: "R$ 7,59",
+      },
+      info3: {
+        title: "Valor",
+        subTitle: "R$ 263,45",
+      },
+      info4: {
+        title: "Últ. Entrada",
+        subTitle: "14/01/2021",
+      },
+      info5: {
+        title: "Últ. Saída",
+        subTitle: "05/03/2021",
+      },
+      info6: {
+        title: "Qtd. Dias",
+        subTitle: "-33",
+      },
+    },
+  };
+};

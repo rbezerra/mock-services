bfc1fe455482c4dd1f33c3bc7f13f691f4c88550
Ramon Bezerra <ramonbezerra90@gmail.com>
2021-07-30 10:34:11 -0300

    incluindo mais produtos

--- functions/return-success.js
+++ functions/return-success.js
@@ -91,6 +91,66 @@ exports.handler = async (event, context) => {
           subTitle: "teste",
         },
       },
+      {
+        title: "Nome do produto 4",
+        subTitle: "309825 | 987654321654 | Mix",
+        imgLink: "https://cdn.awsli.com.br/600x450/914/914004/produto/36238837/21a33a3709.jpg",
+        imgSmallLink: "https://cdn.awsli.com.br/600x450/914/914004/produto/36238837/21a33a3709.jpg",
+        info1: {
+          title: "Estoque",
+          subTitle: "35",
+        },
+        info2: {
+          title: "Custo",
+          subTitle: "R$ 7,59",
+        },
+        info3: {
+          title: "Valor",
+          subTitle: "R$ 263,45",
+        },
+        info4: {
+          title: "Últ. Entrada",
+          subTitle: "14/01/2021",
+        },
+        info5: {
+          title: "Últ. Saída",
+          subTitle: "05/03/2021",
+        },
+        info6: {
+          title: "Teste",
+          subTitle: "teste",
+        },
+      },
+      {
+        title: "Nome do produto 5",
+        subTitle: "309825 | 987654321654 | Mix",
+        imgLink: "https://cdn.awsli.com.br/600x450/914/914004/produto/36238837/21a33a3709.jpg",
+        imgSmallLink: "https://cdn.awsli.com.br/600x450/914/914004/produto/36238837/21a33a3709.jpg",
+        info1: {
+          title: "Estoque",
+          subTitle: "35",
+        },
+        info2: {
+          title: "Custo",
+          subTitle: "R$ 7,59",
+        },
+        info3: {
+          title: "Valor",
+          subTitle: "R$ 263,45",
+        },
+        info4: {
+          title: "Últ. Entrada",
+          subTitle: "14/01/2021",
+        },
+        info5: {
+          title: "Últ. Saída",
+          subTitle: "05/03/2021",
+        },
+        info6: {
+          title: "Teste",
+          subTitle: "teste",
+        },
+      },
     ],
   });
 
